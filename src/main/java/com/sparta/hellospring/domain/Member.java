04ab9141123393667bafc0b7bcd82b591401f20f
SeongKyun <yooss135@naver.com>
2024-06-03 21:08:30 +0900

    MemberService 구현 및 테스트 구현

---
+++ src/main/java/com/sparta/hellospring/domain/Member.java
@@ -0,0 +1,13 @@
+package com.sparta.hellospring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/sparta/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.sparta.hellospring.service;
+
+import com.sparta.hellospring.domain.Member;
+import com.sparta.hellospring.repository.MemberRepository;
+import com.sparta.hellospring.repository.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원 가입
+    public Long join(Member member){
+        //중복 회원 검사
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검사
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
